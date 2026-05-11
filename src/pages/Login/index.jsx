1d50cad94c4df84918319d4db730c35d58c12dda
guraiken <gustavo.fiabani@gmail.com>
2026-05-10 22:40:57 -0300

    style: login bg to gray-200

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -7,7 +7,7 @@ export const Login = () => {
         <div className="w-1/2 bg-[#c23e3e] md:flex justify-center items-center hidden">
             <img src="./public/images/banner-carro.png" alt="" />
         </div>
-        <div className="w-full md:w-1/2 flex justify-center items-center">
+        <div className="w-full md:w-1/2 flex justify-center items-center bg-gray-200">
             <LoginForm/>
         </div>
     </section>
